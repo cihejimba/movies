56fd852d42638869d767b46832de59e4ca0aebaf
Amit Burstein <amit.burstein@gmail.com>
2017-05-26 23:15:14 -0700

    Implement /upcoming API endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,19 @@
+const app = require('express')();
 const functions = require('firebase-functions');
+const request = require('request-promise-native');
+require('dotenv').config();
 
-exports.upcoming = functions.https.onRequest((req, res) => {
-  res.json({});
+const TMDB_ROOT = 'https://api.themoviedb.org/3';
+
+app.get('/api/upcoming', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
+  const options = {
+    uri: TMDB_ROOT + `/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`,
+    json: true,
+  };
+  request(options)
+      .then(data => res.json(data))
+      .catch(error => res.status(error.statusCode).send());
 });
+
+exports.api = functions.https.onRequest(app);
