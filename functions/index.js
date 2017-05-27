d40b7031289d11de57216d891f7dd0360bd9a4b3
Amit Burstein <amit.burstein@gmail.com>
2017-05-26 23:35:28 -0700

    Use Firebase environment variables for Functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
 const app = require('express')();
 const functions = require('firebase-functions');
 const request = require('request-promise-native');
-require('dotenv').config();
 
 const TMDB_ROOT = 'https://api.themoviedb.org/3';
 
 app.get('/api/upcoming', (req, res) => {
   res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
   const options = {
-    uri: TMDB_ROOT + `/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`,
+    uri: TMDB_ROOT + `/movie/upcoming?api_key=${functions.config().tmdb.key}&language=en-US&page=1`,
     json: true,
   };
   request(options)
