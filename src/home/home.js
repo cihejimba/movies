9898ee6a15a4337b523a05ec7a290a9ec07f5fb9
Amit Burstein <amit.burstein@gmail.com>
2017-05-27 18:15:16 -0700

    Fade in movies on home view

--- src/home/home.js
+++ src/home/home.js
@@ -1,3 +1,4 @@
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import Movie from '../movie/movie';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
@@ -10,9 +11,22 @@ export class Home extends Component {
   }
 
   render() {
+    const movies = (
+      <CSSTransitionGroup
+          transitionName="movies-transition"
+          transitionAppearTimeout={500}
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
+          transitionAppear={true}>
+        <div id="movies">
+          {this.props.upcoming.map(movie => <Movie movie={movie} key={movie.id} />)}
+        </div>
+      </CSSTransitionGroup>
+    );
+
     return (
       <div id="home">
-        {this.props.upcoming.map(movie => <Movie movie={movie} key={movie.id} />)}
+        {this.props.upcoming.length ? movies : null}
       </div>
     );
   }
